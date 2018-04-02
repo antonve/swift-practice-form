e43d24f6240e0350fa7db5bf4b043df8bbd3ea62
Anton Van Eechaute <anton@wantedly.com>
2018-04-03 00:49:41 +0900

    It shows a view!

--- app/AppDelegate.swift
+++ app/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let formViewController = UIViewController()
+        formViewController.view.backgroundColor = UIColor.red
+        window!.rootViewController = formViewController
+        window!.makeKeyAndVisible()
+
         return true
     }
 

4f0cab83df27ef63c75d6b52fc47f737109a2a81
Anton Van Eechaute <anton@wantedly.com>
2018-04-03 02:51:59 +0900

    WIP form view

--- app/FormViewController.swift
+++ app/FormViewController.swift
@@ -10,22 +10,67 @@ import UIKit
 import ReactorKit
 import RxSwift
 import RxCocoa
+import SnapKit
 
 class FormViewController: UIViewController, View {
 
     typealias Reactor = FormViewReactor
     var disposeBag = DisposeBag()
 
+    private lazy var stackView: UIStackView = {
+        let subviews = [self.nameFieldView]
+        let view = UIStackView(arrangedSubviews: subviews)
+        view.axis = .vertical
+        view.distribution = .fill
+        return view
+    }()
+
+    private lazy var nameFieldView: UIView = {
+        let label = UILabel()
+        label.text = "Name"
+        label.textColor = .black
+
+        let subviews: [UIView] = [label, nameTextField]
+        let view = UIStackView(arrangedSubviews: subviews)
+        view.axis = .horizontal
+        view.distribution = .fill
+
+        return view
+    }()
+
+    private lazy var nameTextField: UITextField = {
+        let field = UITextField()
+        field.backgroundColor = .red
+        return field
+    }()
+
     func bind(reactor: FormViewReactor) {
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = .yellow
+
+        view.addSubview(stackView)
+
+        setupConstraints()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func setupConstraints() {
+        stackView.snp.makeConstraints {
+            $0.width.height.equalToSuperview().offset(-20)
+            $0.center.equalTo(view.snp.center)
+        }
+
+        nameFieldView.snp.makeConstraints {
+            $0.height.equalTo(44)
+        }
+
+        nameTextField.snp.makeConstraints {
+            $0.width.equalTo(stackView.snp.width).offset(-50)
+        }
     }
+
+
 }

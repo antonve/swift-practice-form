b150908e36b89a0f7e435240f732f26f3f007b03
Anton Van Eechaute <anton@wantedly.com>
2018-04-03 01:33:41 +0900

    Rename view controller

--- app/AppDelegate.swift
+++ app/AppDelegate.swift
@@ -17,8 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
-        let formViewController = UIViewController()
-        formViewController.view.backgroundColor = UIColor.red
+        let formViewController = FormViewController()
         window!.rootViewController = formViewController
         window!.makeKeyAndVisible()
 
